3e6652f90806e62aa99eaa5bdd7e2b6a06a98b60
Taubs99 <connorjwilson99@hotmail.com>
2022-08-08 21:17:20 +1000

    Initial commit

---
+++ src/app/health-counter/health-counter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-health-counter',
+  templateUrl: './health-counter.component.html',
+  styleUrls: ['./health-counter.component.css']
+})
+export class HealthCounterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
