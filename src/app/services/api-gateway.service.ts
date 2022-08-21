a92f3f21a411fb47329fd247af6dd8d489b47ca4
c76-Wilson <n10008276@qut.edu.au>
2022-08-21 14:33:49 +1000

    Adding loader to monster manual

--- src/app/services/api-gateway.service.ts
+++ src/app/services/api-gateway.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http'
 import { environment } from 'src/environments/environment';
-import { tap } from 'rxjs';
+import { catchError, tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -25,5 +25,22 @@ export class ApiGatewayService {
     }
 
     return this.http.get(this.dndOpenApi + `/monsters?page=${page}&search=${searchTerm}`, httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 }
